7b5e52d0d7574f23d6056bd0f6d09914808a0b95
Michael.Samula <msamula@web.de>
2022-10-20 14:31:42 +0200

    0.1

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/particlesCSS.css';
 import '../css/custom.css';
 import particles from'./userInterface/particles';
